347b431fd606df3b46e8f42a2317d5cd734d3136
Jun Noiri <63932655+junnoiri@users.noreply.github.com>
2023-09-05 14:08:18 -0500

    modified the code

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const path = require("path");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 let myVariableServer = "soft coded server data";
 
-app.get("/", (req, res) => {
+app.get("/jun", (req, res) => {
   res.render("index", { myVariableClient: myVariableServer });
 });
 
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/index.html");
-//   res.send("<h1>Hello, World From Express & a Paas/Render!!!</h1>");
-// });
+app.post("/postClientData", (req, res) => {
+  console.log("body: ", req.body);
+  console.log("User Name: ", req.body.userName);
+
+  res.render("index", { myVariableClient: req.body.userName });
+});
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World From Express & a PaaS/Render</h1>");
+});
+
+app.get("/whatever", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
 const port = 3000;
 app.listen(port, () => {
